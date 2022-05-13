c53a31c1d3a8aa4b91e07760c0a59c0ce2ad2967
Alex Bui <buingocan2105@gmail.com>
2022-05-13 18:43:27 +0700

    Initial Commit

---
+++ Jankenpon/JankenponApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenponApp.swift
+//  Jankenpon
+//
+//  Created by Alex Bùi on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct JankenponApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
